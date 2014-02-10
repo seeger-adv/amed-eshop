a863d42667fd40ae3643d5f80ca54c2a95689cff
Jan Seeger <seegerj@adv-boeblingen.de>
2014-02-10 11:04:40 +0100

    add user entity.

--- src/main/java/de/adv_boeblingen/seegerj/amed/eshop/model/User.java
+++ src/main/java/de/adv_boeblingen/seegerj/amed/eshop/model/User.java
@@ -1,8 +1,53 @@
 package de.adv_boeblingen.seegerj.amed.eshop.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity(name = "t_user")
 public class User {
+	@Id
+	@Column(name = "username")
+	private String mUsername;
 
+	@Column(name = "created")
+	private long mCreated;
+
+	@Column(name = "lastlogin")
+	private long mLastLogin;
+
+	@Column(name = "password")
+	private String mPassword;
+
+	public String getUsername() {
+		return this.mUsername;
+	}
+
+	public void setUsername(String username) {
+		this.mUsername = username;
+	}
+
+	public String getPassword() {
+		return this.mPassword;
+	}
+
+	public void setPassword(String password) {
+		this.mPassword = password;
+	}
+
+	public long getCreated() {
+		return this.mCreated;
+	}
+
+	public void setCreated(long mCreated) {
+		this.mCreated = mCreated;
+	}
+
+	public long getLastLogin() {
+		return this.mLastLogin;
+	}
+
+	public void setLastLogin(long mLastLogin) {
+		this.mLastLogin = mLastLogin;
+	}
 }
