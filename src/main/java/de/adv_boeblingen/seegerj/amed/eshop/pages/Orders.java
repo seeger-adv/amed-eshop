06bd198a9a414133133fa843217d87619c5731b6
Jan Seeger <jan@alphadev.net>
2014-03-04 23:13:34 +0100

    renamed MyOrders to Orders.

--- src/main/java/de/adv_boeblingen/seegerj/amed/eshop/pages/Orders.java
+++ src/main/java/de/adv_boeblingen/seegerj/amed/eshop/pages/Orders.java
@@ -3,5 +3,5 @@ package de.adv_boeblingen.seegerj.amed.eshop.pages;
 import de.adv_boeblingen.seegerj.amed.eshop.annotations.RequiresLogin;
 
 @RequiresLogin
-public class MyOrders {
+public class Orders {
 }
