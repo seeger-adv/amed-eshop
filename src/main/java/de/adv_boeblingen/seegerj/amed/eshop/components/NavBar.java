6c85e369ac3c319195b538ff74aa5e75b8e0cc3e
Jan Seeger <jan@alphadev.net>
2014-03-04 18:42:40 +0100

    fix navigation labels.

--- src/main/java/de/adv_boeblingen/seegerj/amed/eshop/components/NavBar.java
+++ src/main/java/de/adv_boeblingen/seegerj/amed/eshop/components/NavBar.java
@@ -2,9 +2,13 @@ package de.adv_boeblingen.seegerj.amed.eshop.components;
 
 import org.apache.tapestry5.ComponentResources;
 import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.ioc.Messages;
 import org.apache.tapestry5.ioc.annotations.Inject;
 
 public class NavBar {
+	@Inject
+	private Messages messages;
+
 	@Inject
 	private ComponentResources resources;
 
@@ -12,7 +16,11 @@ public class NavBar {
 	private String pageName;
 
 	public String[] getPageNames() {
-		return new String[] { "Index", "Products", "About", "Contact" };
+		return new String[] { "Index", "Account", "About", "Contact" };
+	}
+
+	public String getPageLabel() {
+		return messages.get("PAGELABEL_" + pageName);
 	}
 
 	public String getTabClass() {
