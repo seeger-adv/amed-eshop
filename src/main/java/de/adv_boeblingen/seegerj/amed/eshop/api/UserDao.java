1135a02f86a5646e05119a757b74e97e440f897a
Jan Seeger <jan@alphadev.net>
2014-03-15 18:49:50 +0100

    add user dao service.

---
+++ src/main/java/de/adv_boeblingen/seegerj/amed/eshop/api/UserDao.java
@@ -0,0 +1,5 @@
+package de.adv_boeblingen.seegerj.amed.eshop.api;
+
+public interface UserDao {
+	boolean register(final String username, final String password);
+}
---
+++ src/main/java/de/adv_boeblingen/seegerj/amed/eshop/dao/UserDaoImpl.java
@@ -0,0 +1,43 @@
+package de.adv_boeblingen.seegerj.amed.eshop.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.ioc.annotations.InjectService;
+
+import de.adv_boeblingen.seegerj.amed.eshop.api.AuthenticatorService;
+import de.adv_boeblingen.seegerj.amed.eshop.api.CryptService;
+import de.adv_boeblingen.seegerj.amed.eshop.api.UserDao;
+import de.adv_boeblingen.seegerj.amed.eshop.model.Customer;
+import de.adv_boeblingen.seegerj.amed.eshop.provider.DatabaseProvider;
+import de.adv_boeblingen.seegerj.amed.eshop.provider.DatabaseProvider.DatabaseRunnable;
+
+public class UserDaoImpl
+		implements UserDao {
+
+	@InjectService("sha512")
+	private CryptService cryptService;
+
+	@Inject
+	private AuthenticatorService authenticator;
+
+	@Override
+	public boolean register(final String mail, final String password) {
+
+		final Customer customer = new Customer();
+		customer.setMail(mail);
+		String cryptPassword = cryptService.crypt(password);
+		customer.setPassword(cryptPassword);
+
+		DatabaseProvider.runTransaction(new DatabaseRunnable<Void>() {
+			@Override
+			public Void run(EntityManager manager, EntityTransaction transaction) {
+				manager.persist(customer);
+				return null;
+			}
+		});
+
+		return authenticator.login(mail, password);
+	}
+}
