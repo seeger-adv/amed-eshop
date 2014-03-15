a599c127a1b8589018157d397d3b0f253ffbca1e
Jan Seeger <jan@alphadev.net>
2014-03-15 19:41:29 +0100

    add basic account view.

--- src/main/java/de/adv_boeblingen/seegerj/amed/eshop/pages/Account.java
+++ src/main/java/de/adv_boeblingen/seegerj/amed/eshop/pages/Account.java
@@ -1,7 +1,12 @@
 package de.adv_boeblingen.seegerj.amed.eshop.pages;
 
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.ApplicationStateManager;
+
 import de.adv_boeblingen.seegerj.amed.eshop.annotations.RequiresLogin;
 
 @RequiresLogin
 public class Account {
+	@Inject
+	private ApplicationStateManager stateManager;
 }
