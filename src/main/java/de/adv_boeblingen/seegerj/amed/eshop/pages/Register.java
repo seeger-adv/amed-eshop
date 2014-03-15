f53737eddbfdd6995ad116d4cd2ec7a85a79c90c
Jan Seeger <jan@alphadev.net>
2014-03-15 19:09:56 +0100

    add registration form

--- src/main/java/de/adv_boeblingen/seegerj/amed/eshop/pages/Register.java
+++ src/main/java/de/adv_boeblingen/seegerj/amed/eshop/pages/Register.java
@@ -1,8 +1,38 @@
 package de.adv_boeblingen.seegerj.amed.eshop.pages;
 
+import org.apache.tapestry5.annotations.Component;
 import org.apache.tapestry5.annotations.Import;
+import org.apache.tapestry5.annotations.InjectComponent;
+import org.apache.tapestry5.annotations.Persist;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.corelib.components.Form;
+import org.apache.tapestry5.corelib.components.PasswordField;
+import org.apache.tapestry5.ioc.annotations.Inject;
+
+import de.adv_boeblingen.seegerj.amed.eshop.api.UserDao;
 
 @Import(stylesheet = "context:css/login.css")
 public class Register {
 
+	@Persist
+	@Property
+	private String username;
+
+	@Property
+	private String password;
+
+	@Inject
+	private UserDao userDao;
+
+	@InjectComponent
+	private PasswordField passwordField;
+
+	@Component
+	private Form registerForm;
+
+	void onValidateFromRegisterForm() {
+		if (!userDao.register(username, password)) {
+			registerForm.recordError(passwordField, "Invalid user name or password.");
+		}
+	}
 }
