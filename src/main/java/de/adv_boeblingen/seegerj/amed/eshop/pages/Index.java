d26f2a087561c72734116bbf72c6e6f8ca9db52c
Jan Seeger <jan@alphadev.net>
2014-03-16 01:45:11 +0100

    have index retrieve products from the database.

--- src/main/java/de/adv_boeblingen/seegerj/amed/eshop/pages/Index.java
+++ src/main/java/de/adv_boeblingen/seegerj/amed/eshop/pages/Index.java
@@ -1,5 +1,17 @@
 package de.adv_boeblingen.seegerj.amed.eshop.pages;
 
-public class Index {
+import java.util.Set;
 
+import org.apache.tapestry5.ioc.annotations.Inject;
+
+import de.adv_boeblingen.seegerj.amed.eshop.api.Catalog;
+import de.adv_boeblingen.seegerj.amed.eshop.model.Product;
+
+public class Index {
+	@Inject
+	private Catalog catalog;
+
+	public Set<Product> getItems() {
+		return catalog.getItems(null);
+	}
 }
