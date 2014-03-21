df6ea6c79bc4c8fc00aa4e8e14fb8f3a2a8743a9
Jan Seeger <seegerj@adv-boeblingen.de>
2014-03-21 13:03:30 +0100

    fix catalog service ioc declaration.

--- src/main/java/de/adv_boeblingen/seegerj/amed/eshop/services/AppModule.java
+++ src/main/java/de/adv_boeblingen/seegerj/amed/eshop/services/AppModule.java
@@ -7,7 +7,6 @@ import de.adv_boeblingen.seegerj.amed.eshop.api.AuthenticatorService;
 import de.adv_boeblingen.seegerj.amed.eshop.api.Catalog;
 import de.adv_boeblingen.seegerj.amed.eshop.api.CryptService;
 import de.adv_boeblingen.seegerj.amed.eshop.api.UserDao;
-import de.adv_boeblingen.seegerj.amed.eshop.dao.CatalogDao;
 import de.adv_boeblingen.seegerj.amed.eshop.dao.UserDaoImpl;
 import de.adv_boeblingen.seegerj.amed.eshop.filters.RequiresLoginFilter;
 
@@ -22,6 +21,6 @@ public class AppModule {
 		binder.bind(CryptService.class, MD5Service.class);
 
 		binder.bind(UserDao.class, UserDaoImpl.class);
-		binder.bind(Catalog.class, CatalogDao.class);
+		binder.bind(Catalog.class, CatalogServiceImpl.class);
 	}
 }
