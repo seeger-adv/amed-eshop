dbbff7b4f898c2fda14dbc2215c9890c457627fd
Jan Seeger <seegerj@adv-boeblingen.de>
2014-03-21 12:53:46 +0100

    add basic catalog service.

---
+++ src/main/java/de/adv_boeblingen/seegerj/amed/eshop/services/CatalogServiceImpl.java
@@ -0,0 +1,38 @@
+package de.adv_boeblingen.seegerj.amed.eshop.services;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import de.adv_boeblingen.seegerj.amed.eshop.api.Catalog;
+import de.adv_boeblingen.seegerj.amed.eshop.api.Filter;
+import de.adv_boeblingen.seegerj.amed.eshop.model.Product;
+import de.adv_boeblingen.seegerj.amed.eshop.provider.DatabaseProvider;
+import de.adv_boeblingen.seegerj.amed.eshop.provider.DatabaseProvider.DatabaseRunnable;
+
+public class CatalogServiceImpl implements Catalog {
+	@Override
+	public Set<Product> getProducts(final Filter filter) {
+		return DatabaseProvider.runQuery(new DatabaseRunnable<Set<Product>>() {
+			@Override
+			public Set<Product> run(EntityManager manager, EntityTransaction transaction) {
+				CriteriaBuilder builder = manager.getCriteriaBuilder();
+				CriteriaQuery<Product> criteria = builder.createQuery(Product.class);
+
+				if (filter != null) {
+					Root<Product> root = criteria.from(Product.class);
+					filter.filter(null);
+				}
+
+				TypedQuery<Product> typedQuery = manager.createQuery(criteria);
+				return new HashSet<Product>(typedQuery.getResultList());
+			}
+		});
+	}
+}
