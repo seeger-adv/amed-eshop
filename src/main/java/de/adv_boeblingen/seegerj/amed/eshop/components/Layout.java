54d2299a7908fa28c9620c5e4d47951cfdc7fc16
Jan Seeger <jan@alphadev.net>
2014-03-23 14:11:49 +0100

    fix bootstrap loading.

--- src/main/java/de/adv_boeblingen/seegerj/amed/eshop/components/Layout.java
+++ src/main/java/de/adv_boeblingen/seegerj/amed/eshop/components/Layout.java
@@ -1,8 +1,6 @@
 package de.adv_boeblingen.seegerj.amed.eshop.components;
 
-import org.apache.tapestry5.annotations.Import;
 
-@Import(stylesheet = { "context:css/bootstrap.min.css" })
 public class Layout {
 
 }
--- src/main/java/de/adv_boeblingen/seegerj/amed/eshop/components/ShoppingCart.java
+++ src/main/java/de/adv_boeblingen/seegerj/amed/eshop/components/ShoppingCart.java
@@ -9,6 +9,7 @@ import org.apache.tapestry5.annotations.SessionState;
 import de.adv_boeblingen.seegerj.amed.eshop.model.Cart;
 import de.adv_boeblingen.seegerj.amed.eshop.model.database.Item;
 
+@Import(library = { "context:js/dropdown.js" })
 public class ShoppingCart {
 	@Property
 	@SessionState
