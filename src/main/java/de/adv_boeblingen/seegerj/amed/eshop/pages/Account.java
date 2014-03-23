a02e06ef6ef7431c1a560cdb34bd02751877d7f7
Jan Seeger <jan@alphadev.net>
2014-03-23 12:55:51 +0100

    show previous purchases under my account.

--- src/main/java/de/adv_boeblingen/seegerj/amed/eshop/pages/Account.java
+++ src/main/java/de/adv_boeblingen/seegerj/amed/eshop/pages/Account.java
@@ -1,12 +1,24 @@
 package de.adv_boeblingen.seegerj.amed.eshop.pages;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.services.ApplicationStateManager;
 
 import de.adv_boeblingen.seegerj.amed.eshop.annotations.RequiresLogin;
+import de.adv_boeblingen.seegerj.amed.eshop.model.database.Purchase;
 
 @RequiresLogin
 public class Account {
 	@Inject
 	private ApplicationStateManager stateManager;
+
+	@Property
+	private Purchase purchase;
+
+	public Set<Purchase> getPurchases() {
+		return new HashSet<Purchase>();
+	}
 }
