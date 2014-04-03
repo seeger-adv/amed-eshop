614b72aa8c81e5348f39d52c0b2fc779508e2861
Jan Seeger <seegerj@adv-boeblingen.de>
2014-04-03 11:10:03 +0200

    add search field to layout. fix issue #42.

---
+++ src/main/java/de/adv_boeblingen/seegerj/amed/eshop/components/SearchField.java
@@ -0,0 +1,30 @@
+package de.adv_boeblingen.seegerj.amed.eshop.components;
+
+import javax.inject.Inject;
+
+import org.apache.tapestry5.Link;
+import org.apache.tapestry5.annotations.Component;
+import org.apache.tapestry5.annotations.Parameter;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.corelib.components.Form;
+import org.apache.tapestry5.services.PageRenderLinkSource;
+
+public class SearchField {
+	@Property
+	private String query;
+
+	@Component
+	private Form searchForm;
+
+	@Inject
+	private PageRenderLinkSource linksource;
+
+	@Property
+	@Parameter(defaultPrefix = "literal")
+	private String layout;
+
+	Link onSuccess() {
+		return this.linksource.createPageRenderLinkWithContext(
+				de.adv_boeblingen.seegerj.amed.eshop.pages.Category.class, this.query);
+	}
+}
