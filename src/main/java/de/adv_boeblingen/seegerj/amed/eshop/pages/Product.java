1c52fc5380d185d15e3f7f22959a8fd2fee4203d
Jan Seeger <seegerj@adv-boeblingen.de>
2014-04-03 16:36:48 +0200

    proper pdocut display *without* fancy styling.

--- src/main/java/de/adv_boeblingen/seegerj/amed/eshop/pages/Product.java
+++ src/main/java/de/adv_boeblingen/seegerj/amed/eshop/pages/Product.java
@@ -1,6 +1,8 @@
 package de.adv_boeblingen.seegerj.amed.eshop.pages;
 
+import org.apache.tapestry5.annotations.Component;
 import org.apache.tapestry5.annotations.Persist;
+import org.apache.tapestry5.corelib.components.EventLink;
 import org.apache.tapestry5.ioc.annotations.Inject;
 
 import de.adv_boeblingen.seegerj.amed.eshop.api.ProductDao;
@@ -9,14 +11,25 @@ public class Product {
 	@Inject
 	private ProductDao catalog;
 
+	@Component(parameters = { "event=add", "context=product.id" })
+	private EventLink addToBasket;
+
 	@Persist
 	private de.adv_boeblingen.seegerj.amed.eshop.model.database.Product product;
 
 	public de.adv_boeblingen.seegerj.amed.eshop.model.database.Product getProduct() {
-		return product;
+		return this.product;
+	}
+
+	public String getAvailability() {
+		return "Available";
 	}
 
 	void onActivate(long productId) {
-		product = catalog.getProduct(productId);
+		this.product = this.catalog.getProduct(productId);
+	}
+
+	void onAdd(String productId) {
+		System.out.println("added Product(Id:" + productId + ") to Shopping Cart");
 	}
 }
