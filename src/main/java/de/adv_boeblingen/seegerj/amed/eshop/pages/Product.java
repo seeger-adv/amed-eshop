f7d50c9afdd364c0bb12810a0b7918907ddd628d
Jan Seeger <seegerj@adv-boeblingen.de>
2014-04-04 11:28:56 +0200

    impl add to cart.

--- src/main/java/de/adv_boeblingen/seegerj/amed/eshop/pages/Product.java
+++ src/main/java/de/adv_boeblingen/seegerj/amed/eshop/pages/Product.java
@@ -2,10 +2,14 @@ package de.adv_boeblingen.seegerj.amed.eshop.pages;
 
 import org.apache.tapestry5.annotations.Component;
 import org.apache.tapestry5.annotations.Persist;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.annotations.SessionState;
 import org.apache.tapestry5.corelib.components.EventLink;
 import org.apache.tapestry5.ioc.annotations.Inject;
 
 import de.adv_boeblingen.seegerj.amed.eshop.api.ProductDao;
+import de.adv_boeblingen.seegerj.amed.eshop.model.Cart;
+import de.adv_boeblingen.seegerj.amed.eshop.model.database.Item;
 
 public class Product {
 	@Inject
@@ -16,6 +20,10 @@ public class Product {
 
 	@Persist
 	private de.adv_boeblingen.seegerj.amed.eshop.model.database.Product product;
+
+	@Property
+	@SessionState
+	private Cart shoppingCart;
 
 	public de.adv_boeblingen.seegerj.amed.eshop.model.database.Product getProduct() {
 		return this.product;
@@ -30,6 +38,9 @@ public class Product {
 	}
 
 	void onAdd(String productId) {
-		System.out.println("added Product(Id:" + productId + ") to Shopping Cart");
+		Item item = new Item();
+		item.setProduct(this.product);
+		item.setAmount(1);
+		this.shoppingCart.getItems().add(item);
 	}
 }
