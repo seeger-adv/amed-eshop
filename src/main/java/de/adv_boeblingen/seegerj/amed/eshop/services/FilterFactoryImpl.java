adffbe1a88978405b6a823bef8686f84d30367dc
Jan Seeger <jan@alphadev.net>
2014-04-06 00:56:41 +0200

    cleanup FilterFactoryImpl.

--- src/main/java/de/adv_boeblingen/seegerj/amed/eshop/services/FilterFactoryImpl.java
+++ src/main/java/de/adv_boeblingen/seegerj/amed/eshop/services/FilterFactoryImpl.java
@@ -12,10 +12,16 @@ import de.adv_boeblingen.seegerj.amed.eshop.filters.SubcategoryFilter;
 import de.adv_boeblingen.seegerj.amed.eshop.model.database.Category;
 import de.adv_boeblingen.seegerj.amed.eshop.model.database.Product;
 
-public class FilterFactoryImpl implements FilterFactory {
+public class FilterFactoryImpl
+		implements FilterFactory {
+
+	public static final String RECOMMENDED = "fav";
+	public static final String CATEGORY = "cat";
+	public static final String NULL = "null";
+	public static final String SEARCH = "query";
+
 	@Override
 	public Filter<Category> getCategoryFilter(String parameters) {
-		System.out.println("Category: " + parameters);
 		StringTokenizer tokenizer = new StringTokenizer(parameters, ":");
 
 		if (!tokenizer.hasMoreElements()) {
@@ -23,11 +29,11 @@ public class FilterFactoryImpl implements FilterFactory {
 		}
 
 		String command = tokenizer.nextToken();
-		if ("cat".equals(command)) {
+		if (CATEGORY.equals(command)) {
 			String cat = tokenizer.nextToken();
 			long categoryId = Long.parseLong(cat);
 			return new SubcategoryFilter(categoryId);
-		} else if ("null".equals(command)) {
+		} else if (NULL.equals(command)) {
 			return new NullCategoryFilter();
 		}
 
@@ -36,7 +42,6 @@ public class FilterFactoryImpl implements FilterFactory {
 
 	@Override
 	public Filter<Product> getProductFilter(String parameters) {
-		System.out.println("Product: " + parameters);
 		StringTokenizer tokenizer = new StringTokenizer(parameters, ":");
 
 		if (!tokenizer.hasMoreElements()) {
@@ -44,13 +49,13 @@ public class FilterFactoryImpl implements FilterFactory {
 		}
 
 		String command = tokenizer.nextToken();
-		if ("cat".equals(command)) {
+		if (CATEGORY.equals(command)) {
 			String cat = tokenizer.nextToken();
 			long categoryId = Long.parseLong(cat);
 			return new CategoryFilter(categoryId);
-		} else if ("query".equals(command)) {
+		} else if (SEARCH.equals(command)) {
 			return new SearchFilter(tokenizer.nextToken());
-		} else if ("fav".equals(command)) {
+		} else if (RECOMMENDED.equals(command)) {
 			return null; // new FavFilter();
 		}
 
