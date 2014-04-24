919e2fbf6ac38a16f146b1dc32eeb018c6d39607
Jan Seeger <jan@alphadev.net>
2014-04-24 14:10:56 +0200

    add credit card strategy to payment model.

---
+++ src/main/java/de/adv_boeblingen/seegerj/amed/eshop/model/payment/CreditCard.java
@@ -0,0 +1,42 @@
+package de.adv_boeblingen.seegerj.amed.eshop.model.payment;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+public class CreditCard
+		extends PaymentInfo {
+
+	@Column
+	private String owner;
+
+	@Column
+	private String number;
+
+	@Column
+	private String cvv;
+
+	public String getOwner() {
+		return owner;
+	}
+
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public String getCvv() {
+		return cvv;
+	}
+
+	public void setCvv(String cvv) {
+		this.cvv = cvv;
+	}
+}
