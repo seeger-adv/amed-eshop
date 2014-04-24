948331b42aae83ff61645282765480c6c360074a
Jan Seeger <jan@alphadev.net>
2014-04-24 14:12:08 +0200

    add Checkout Page.

--- src/main/java/de/adv_boeblingen/seegerj/amed/eshop/pages/Checkout.java
+++ src/main/java/de/adv_boeblingen/seegerj/amed/eshop/pages/Checkout.java
@@ -1,8 +1,45 @@
 package de.adv_boeblingen.seegerj.amed.eshop.pages;
 
+import java.util.Set;
+
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.ApplicationStateManager;
+
 import de.adv_boeblingen.seegerj.amed.eshop.annotations.RequiresLogin;
+import de.adv_boeblingen.seegerj.amed.eshop.model.Session;
+import de.adv_boeblingen.seegerj.amed.eshop.model.database.Address;
+import de.adv_boeblingen.seegerj.amed.eshop.model.database.Customer;
+import de.adv_boeblingen.seegerj.amed.eshop.model.payment.PaymentInfo;
 
 @RequiresLogin
 public class Checkout {
+
+	@Inject
+	private ApplicationStateManager stateManager;
+
+	private Session session;
+	private Set<PaymentInfo> paymentInfo;
+	private Set<Address> addresses;
+
+	public Object onActivate() {
+		session = stateManager.get(Session.class);
+
+		Customer customer = session.getCustomer();
+		if (customer == null) {
+			return Login.class;
+		}
+
+		paymentInfo = customer.getPaymentInfo();
+		if (paymentInfo.isEmpty()) {
+			return AddPayment.class;
+		}
+
+		addresses = customer.getAddress();
+		if (addresses.isEmpty()) {
+			return AddAddress.class;
+		}
+
+		return null;
+	}
 
 }
