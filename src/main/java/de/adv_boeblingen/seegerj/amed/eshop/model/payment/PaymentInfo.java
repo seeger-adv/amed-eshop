f81b019cccf30eb515ea54b1f3350e5bec7dce89
Jan Seeger <jan@alphadev.net>
2014-04-25 12:46:54 +0200

    fix payment info table.

--- src/main/java/de/adv_boeblingen/seegerj/amed/eshop/model/payment/PaymentInfo.java
+++ src/main/java/de/adv_boeblingen/seegerj/amed/eshop/model/payment/PaymentInfo.java
@@ -2,6 +2,8 @@ package de.adv_boeblingen.seegerj.amed.eshop.model.payment;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
@@ -10,6 +12,7 @@ import javax.persistence.InheritanceType;
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class PaymentInfo {
 	@Id
+	@GeneratedValue(strategy = GenerationType.TABLE)
 	@Column
 	private long id;
 }
