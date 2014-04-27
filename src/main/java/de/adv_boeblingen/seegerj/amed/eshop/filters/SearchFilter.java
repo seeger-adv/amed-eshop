22ea44c67493ef0bdfdf1d788e7830ee2bf16b6b
Jan Seeger <jan@alphadev.net>
2014-04-27 13:25:41 +0200

    also search the title for matches instead of just the description.

--- src/main/java/de/adv_boeblingen/seegerj/amed/eshop/filters/SearchFilter.java
+++ src/main/java/de/adv_boeblingen/seegerj/amed/eshop/filters/SearchFilter.java
@@ -4,12 +4,14 @@ import javax.persistence.EntityManager;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Path;
+import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
 import de.adv_boeblingen.seegerj.amed.eshop.api.Filter;
 import de.adv_boeblingen.seegerj.amed.eshop.model.database.Product;
 
-public class SearchFilter implements Filter<Product> {
+public class SearchFilter
+		implements Filter<Product> {
 
 	private final String query;
 
@@ -20,7 +22,10 @@ public class SearchFilter implements Filter<Product> {
 	@Override
 	public void filter(EntityManager em, CriteriaBuilder cb, Root<Product> root, CriteriaQuery<Product> query) {
 		String searchTerm = "%" + this.query + "%";
-		Path<String> field = root.<String> get("description");
-		query.where(cb.like(field, searchTerm));
+		Path<String> description = root.<String> get("description");
+		Path<String> slug = root.<String> get("slug");
+		Predicate descPredicate = cb.like(description, searchTerm);
+		Predicate slugPredicate = cb.like(slug, searchTerm);
+		query.where(cb.or(descPredicate, slugPredicate));
 	}
 }
