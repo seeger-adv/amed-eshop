8112cd52e6fed1cfda75873b700683cf28755795
Jan Seeger <jan@alphadev.net>
2014-04-27 14:36:39 +0200

    clean up Checkout page.

--- src/main/java/de/adv_boeblingen/seegerj/amed/eshop/pages/Checkout.java
+++ src/main/java/de/adv_boeblingen/seegerj/amed/eshop/pages/Checkout.java
@@ -1,17 +1,16 @@
 package de.adv_boeblingen.seegerj.amed.eshop.pages;
 
-import java.util.Set;
-
 import org.apache.tapestry5.Link;
+import org.apache.tapestry5.annotations.Component;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.corelib.components.EventLink;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.services.ApplicationStateManager;
 import org.apache.tapestry5.services.PageRenderLinkSource;
 
 import de.adv_boeblingen.seegerj.amed.eshop.annotations.RequiresLogin;
 import de.adv_boeblingen.seegerj.amed.eshop.model.Session;
-import de.adv_boeblingen.seegerj.amed.eshop.model.database.Address;
 import de.adv_boeblingen.seegerj.amed.eshop.model.database.Customer;
-import de.adv_boeblingen.seegerj.amed.eshop.model.payment.PaymentInfo;
 
 @RequiresLogin
 public class Checkout {
@@ -23,27 +22,25 @@ public class Checkout {
 	private PageRenderLinkSource renderLinkSource;
 
 	private Session session;
-	private Set<PaymentInfo> paymentInfo;
-	private Set<Address> addresses;
+
+	@Property
+	private Customer customer;
 
 	public Object onActivate() {
 		session = stateManager.get(Session.class);
 
-		Customer customer = session.getCustomer();
+		customer = session.getCustomer();
 		if (customer == null) {
 			return Login.class;
 		}
 
 		Link link = null;
-
-		addresses = customer.getAddress();
-		if (addresses.isEmpty()) {
+		if (customer.getAddress().isEmpty()) {
 			link = this.renderLinkSource.createPageRenderLink(AddAddress.class);
 			link.addParameterValue("next", "Checkout");
 		}
 
-		paymentInfo = customer.getPaymentInfo();
-		if (paymentInfo.isEmpty()) {
+		if (customer.getPaymentInfo().isEmpty()) {
 			link = this.renderLinkSource.createPageRenderLink(AddPayment.class);
 			link.addParameterValue("next", "Checkout");
 		}
