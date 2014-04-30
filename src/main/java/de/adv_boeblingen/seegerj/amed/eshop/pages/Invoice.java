fc453c45c2d9c244484f075dcd02aec35121f5ab
Jan Seeger <seegerj@adv-boeblingen.de>
2014-04-30 12:55:57 +0200

    add invoice page..
    partially implemented the pdf generation.

---
+++ src/main/java/de/adv_boeblingen/seegerj/amed/eshop/pages/Invoice.java
@@ -0,0 +1,46 @@
+package de.adv_boeblingen.seegerj.amed.eshop.pages;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.tapestry5.StreamResponse;
+import org.apache.tapestry5.services.Response;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.pdf.PdfWriter;
+
+public class Invoice {
+	public StreamResponse onAction() {
+		return new StreamResponse() {
+			@Override
+			public String getContentType() {
+				return "application/pdf";
+			}
+
+			@Override
+			public InputStream getStream() throws IOException {
+				return createInvoice();
+			}
+
+			@Override
+			public void prepareResponse(Response response) {
+				response.setHeader("Content-disposition", "attachment;filename=Invoice.pdf");
+			}
+		};
+	}
+
+	public InputStream createInvoice() {
+		Document document = new Document(PageSize.A4);
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		try {
+			PdfWriter.getInstance(document, baos);
+			document.open();
+		} catch (Exception e) {
+
+		} finally {
+			baos.close();
+		}
+	}
+}
