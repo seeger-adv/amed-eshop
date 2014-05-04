11f5f884c36af63d48066a8cc81e7138dbf86a9d
Jan Seeger <jan@alphadev.net>
2014-05-04 19:34:56 +0200

    refactored checkout pages.

--- src/main/java/de/adv_boeblingen/seegerj/amed/eshop/pages/CheckoutAddress.java
+++ src/main/java/de/adv_boeblingen/seegerj/amed/eshop/pages/CheckoutAddress.java
@@ -2,42 +2,18 @@ package de.adv_boeblingen.seegerj.amed.eshop.pages;
 
 import java.util.Set;
 
-import org.apache.tapestry5.annotations.Property;
-import org.apache.tapestry5.ioc.annotations.Inject;
-import org.apache.tapestry5.services.ApplicationStateManager;
-
 import de.adv_boeblingen.seegerj.amed.eshop.annotations.RequiresLogin;
-import de.adv_boeblingen.seegerj.amed.eshop.model.Session;
 import de.adv_boeblingen.seegerj.amed.eshop.model.database.Address;
-import de.adv_boeblingen.seegerj.amed.eshop.model.database.Purchase;
 
 @RequiresLogin
-public class CheckoutAddress {
-	@Inject
-	private ApplicationStateManager stateManager;
+public class CheckoutAddress extends AbstractCheckoutSelection<Address> {
+	@Override
+	protected void add(Address a) {
+		purchase.setAddress(a);
+	}
 
-	private Session session;
-
-	private Purchase purchase;
-
-	@Property
-	private Address address;
-
-	public Set<Address> getAddresses() {
+	@Override
+	public Set<Address> getItems() {
 		return session.getCustomer().getAddress();
-	}
-
-	public Object onActionFromSelect(long addressId) {
-		for (Address a : getAddresses()) {
-			if (a.getId() == addressId) {
-				purchase.setAddress(a);
-			}
-		}
-		return Checkout.class;
-	}
-
-	public void onActivate() {
-		session = stateManager.get(Session.class);
-		purchase = stateManager.get(Purchase.class);
 	}
 }
--- src/main/java/de/adv_boeblingen/seegerj/amed/eshop/pages/CheckoutPayment.java
+++ src/main/java/de/adv_boeblingen/seegerj/amed/eshop/pages/CheckoutPayment.java
@@ -2,42 +2,18 @@ package de.adv_boeblingen.seegerj.amed.eshop.pages;
 
 import java.util.Set;
 
-import org.apache.tapestry5.annotations.Property;
-import org.apache.tapestry5.ioc.annotations.Inject;
-import org.apache.tapestry5.services.ApplicationStateManager;
-
 import de.adv_boeblingen.seegerj.amed.eshop.annotations.RequiresLogin;
-import de.adv_boeblingen.seegerj.amed.eshop.model.Session;
-import de.adv_boeblingen.seegerj.amed.eshop.model.database.Purchase;
 import de.adv_boeblingen.seegerj.amed.eshop.model.payment.PaymentInfo;
 
 @RequiresLogin
-public class CheckoutPayment {
-	@Inject
-	private ApplicationStateManager stateManager;
+public class CheckoutPayment extends AbstractCheckoutSelection<PaymentInfo> {
+	@Override
+	protected void add(PaymentInfo a) {
+		purchase.setPaymentInfo(a);
+	}
 
-	private Session session;
-
-	private Purchase purchase;
-
-	@Property
-	private PaymentInfo payment;
-
-	public Set<PaymentInfo> getPaymentInfos() {
+	@Override
+	public Set<PaymentInfo> getItems() {
 		return session.getCustomer().getPaymentInfo();
-	}
-
-	public Object onActionFromSelect(long paymentid) {
-		for (PaymentInfo p : getPaymentInfos()) {
-			if (p.getId() == paymentid) {
-				purchase.setPaymentInfo(p);
-			}
-		}
-		return Checkout.class;
-	}
-
-	public void onActivate() {
-		session = stateManager.get(Session.class);
-		purchase = stateManager.get(Purchase.class);
 	}
 }
