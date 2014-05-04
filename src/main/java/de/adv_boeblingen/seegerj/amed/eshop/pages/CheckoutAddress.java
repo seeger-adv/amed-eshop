d4c0b5396cc861f222a82012fac43798cbc18dd4
Jan Seeger <jan@alphadev.net>
2014-05-04 14:16:41 +0200

    add code to provide the CheckoutAddress/CheckoutPayment information to the underlying views.

--- src/main/java/de/adv_boeblingen/seegerj/amed/eshop/pages/CheckoutAddress.java
+++ src/main/java/de/adv_boeblingen/seegerj/amed/eshop/pages/CheckoutAddress.java
@@ -1,8 +1,40 @@
 package de.adv_boeblingen.seegerj.amed.eshop.pages;
 
+import java.util.Set;
+
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.ApplicationStateManager;
+
 import de.adv_boeblingen.seegerj.amed.eshop.annotations.RequiresLogin;
+import de.adv_boeblingen.seegerj.amed.eshop.model.Session;
+import de.adv_boeblingen.seegerj.amed.eshop.model.database.Address;
+import de.adv_boeblingen.seegerj.amed.eshop.model.database.Purchase;
 
 @RequiresLogin
 public class CheckoutAddress {
+	@Inject
+	private ApplicationStateManager stateManager;
 
+	private Session session;
+
+	private Purchase purchase;
+
+	@Property
+	private Address address;
+
+	public Set<Address> getAddresses() {
+		return session.getCustomer().getAddress();
+	}
+
+	public void onActivate() {
+		session = stateManager.get(Session.class);
+		purchase = stateManager.get(Purchase.class);
+	}
+
+	public Object onSelect(Address address) {
+		System.out.println(address);
+		purchase.setAddress(address);
+		return Checkout.class;
+	}
 }
--- src/main/java/de/adv_boeblingen/seegerj/amed/eshop/pages/CheckoutPayment.java
+++ src/main/java/de/adv_boeblingen/seegerj/amed/eshop/pages/CheckoutPayment.java
@@ -1,8 +1,41 @@
 package de.adv_boeblingen.seegerj.amed.eshop.pages;
 
+import java.util.Set;
+
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.ApplicationStateManager;
+
 import de.adv_boeblingen.seegerj.amed.eshop.annotations.RequiresLogin;
+import de.adv_boeblingen.seegerj.amed.eshop.model.Session;
+import de.adv_boeblingen.seegerj.amed.eshop.model.database.Address;
+import de.adv_boeblingen.seegerj.amed.eshop.model.database.Purchase;
+import de.adv_boeblingen.seegerj.amed.eshop.model.payment.PaymentInfo;
 
 @RequiresLogin
 public class CheckoutPayment {
+	@Inject
+	private ApplicationStateManager stateManager;
 
+	private Session session;
+
+	private Purchase purchase;
+
+	@Property
+	private Address address;
+
+	public Set<PaymentInfo> getPaymentInfos() {
+		return session.getCustomer().getPaymentInfo();
+	}
+
+	public void onActivate() {
+		session = stateManager.get(Session.class);
+		purchase = stateManager.get(Purchase.class);
+	}
+
+	public Object onSelect(PaymentInfo payment) {
+		System.out.println(payment);
+		purchase.setPaymentInfo(payment);
+		return Checkout.class;
+	}
 }
