4d901b73289716f9977ecf9a6186a68dc0c0f36e
Jan Seeger <seegerj@adv-boeblingen.de>
2014-05-23 11:10:21 +0200

    add contact form. fix issue #10.

--- src/main/java/de/adv_boeblingen/seegerj/amed/eshop/pages/Contact.java
+++ src/main/java/de/adv_boeblingen/seegerj/amed/eshop/pages/Contact.java
@@ -1,5 +1,51 @@
 package de.adv_boeblingen.seegerj.amed.eshop.pages;
 
-public class Contact {
+import org.apache.tapestry5.annotations.Component;
+import org.apache.tapestry5.annotations.Import;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.corelib.components.Form;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.ApplicationStateManager;
+import org.apache.tapestry5.services.ComponentSource;
 
+import de.adv_boeblingen.seegerj.amed.eshop.model.Session;
+import de.adv_boeblingen.seegerj.amed.eshop.model.database.Customer;
+
+@Import(stylesheet = "context:css/contact.css")
+public class Contact {
+	@Property
+	private String username;
+
+	@Property
+	private String message;
+
+	@Inject
+	private ComponentSource componentSource;
+
+	@Inject
+	private ApplicationStateManager stateManager;
+
+	@Component
+	private Form contactForm;
+
+	public void setupRender() {
+		this.username = getUserInfo();
+	}
+
+	void onValidateFromContactForm() {
+		System.out.println("Message processed.");
+	}
+
+	Object onSuccess() {
+		return Contact.class;
+	}
+
+	public String getUserInfo() {
+		Session session = this.stateManager.get(Session.class);
+		Customer user = session.getCustomer();
+		if (user == null) {
+			return null;
+		}
+		return user.getMail();
+	}
 }
